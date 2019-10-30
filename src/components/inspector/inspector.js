b05fe393d7b326cc89657ea3a44967fa8357d274
morgan <morgan.o@inflo.ai>
2019-10-30 20:54:30 +0000

    add category promote category delete category add entry css updates

--- src/components/inspector/inspector.js
+++ src/components/inspector/inspector.js
@@ -1,4 +1,4 @@
-import React, {useContext, useState, useEffect} from 'react';
+import React, {useContext, useState, useEffect, useRef} from 'react';
 import DictionaryContext from "../../contexts/dictionary.context";
 
 import _ from 'lodash';
@@ -7,23 +7,66 @@ import Entry from "./Entry";
 import './inspector.scss'
 
 function Inspector() {
-    let {dictionary, activePath} = useContext(DictionaryContext);
+    let {dictionary, activePath, dispatchDictionaryAction} = useContext(DictionaryContext);
     const [entries, setEntries] = useState(null);
     const [keyRoot, setKeyRoot] = useState('');
-    useEffect(() =>{
-        let found = _.get(dictionary,activePath);
-        if(Array.isArray(found)) {
-            setEntries(found);
-        }
-    },[activePath, dictionary]);
+    const [name, setName] = useState('');
+    const nameRef = useRef(null);
+    const entryRef = useRef(null);
+    useEffect(() => {
+        let found = _.get(dictionary, activePath);
+        setEntries(Array.isArray(found) ? found : null);
+    }, [activePath, dictionary]);
 
-    useEffect(()=>{
+    useEffect(() => {
         setKeyRoot(activePath);
-    },[activePath]);
+        let parts = activePath.split('.');
+        let newName = parts[parts.length - 1];
+        setName(newName);
+        nameRef.current.value = newName;
+    }, [activePath]);
+
+    function updateName(e) {
+        e.preventDefault();
+        dispatchDictionaryAction({type: 'renameCategory', path: keyRoot, name: nameRef.current.value});
+    }
+
+    function deleteCategory(e) {
+        e.preventDefault();
+        dispatchDictionaryAction({type: 'deleteCategory', path: keyRoot});
+    }
+
+    function promoteCategory(e) {
+        e.preventDefault();
+        dispatchDictionaryAction({type: 'promoteCategory', path: keyRoot});
+    }
+
+    function addEntry(e) {
+        e.preventDefault();
+        dispatchDictionaryAction({type: 'addEntry', path: keyRoot, name:entryRef.current.value});
+        entryRef.current.value = '';
+    }
+
 
     return <div className="inspector">
-        <h1 className="inspector__title">{activePath}</h1>
-        {entries && entries.map((entry,i)=><Entry key={`${keyRoot}.${i}`} entry={entry} path={activePath} index={i}/>)}
+        <h1 className="inspector__title">{activePath.replace('entries.','')}</h1>
+        <div className="inspector__controls">
+            <form onSubmit={updateName}>
+                <input placeholder={name} onBlur={updateName} ref={nameRef}/>
+            </form>
+                <button onClick={deleteCategory}>delete</button>
+                {entries && <button onClick={promoteCategory}>promote</button>}
+
+        </div>
+        {entries && <div className="inspector__entries">
+            {entries.map((entry, i) =>
+                <Entry key={`${keyRoot}.${i}`} entry={entry} path={activePath} index={i}/>
+            )}
+            <form onSubmit={addEntry}>
+                <input ref={entryRef} placeholder="new entry" onBlur={addEntry}/>
+            </form>
+        </div>
+        }
     </div>
 }
 
